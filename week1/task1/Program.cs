1e404c6f22e46b042d7f96d449ccdfc60b212fd2
zitasanova00 <47642594+zitasanova00@users.noreply.github.com>
2019-02-15 00:45:23 +0500

    Add files via upload

---
+++ week1/task1/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace Task1
+{
+    class Program
+    {
+        public static bool isPrime(int x)
+        {
+            if (x < 2)
+                return false;
+            for (int j = 2; j <= Math.Sqrt(x); ++j) // Easy Algorithm O(sqrt(x))
+                if (x % j == 0)
+                    return false;
+            return true;
+        }
+        static void Main(string[] args)
+        {
+            int n;
+            n = Convert.ToInt32(Console.ReadLine()); // Reading line and converting to Int
+            int[] a = new int[n]; // opening array with n length
+            int cnt = 0, x;
+            string[] numbers = Console.ReadLine().Split(); // Reading line and split
+            for (int i = 0; i < n; ++i) // same as C++
+            {
+                x = Convert.ToInt32(numbers[i]);
+                if (isPrime(x)) // Checking is x prime or not by function
+                    a[cnt++] = x;
+            }
+            Console.WriteLine(cnt);
+            for (int i = 0; i < cnt; ++i)
+            {
+                Console.Write(a[i].ToString() + " "); // Output answers in string form
+            }
+        }
+    }
+}
---
+++ week1/task2/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+public class Student
+{
+    private string name; // приватная перемена name
+    private string id;
+    private int year;
+    public Student(string name, string id, int year) // Конструктор с 3 параметрами
+    {
+        this.name = name; // чтобы между 2-мя переменами не было конфликта 
+        this.id = id;
+        this.year = year;
+    }
+    public Student() // Конструктор с 0 параметрами
+    {
+        string[] reading = Console.ReadLine().Split(); // Читаем и делим по пробелам
+        while (reading.Length != 3)
+        { // Будем считывать пока не будет 3 ввода
+            Console.WriteLine("Wrong Input, please input correctly: name id year");
+            reading = Console.ReadLine().Split(); // Обновляем вводные данные
+        }
+        // reading = {Carl, 18BD, 2018}
+        this.name = reading[0]; // Carl
+        this.id = reading[1]; // 18BD
+        this.year = Convert.ToInt32(reading[2]); // 2018
+    }
+    public void access() // Метод который возвращает данные Студента
+    {
+        Console.WriteLine(name + " " + id + " " + (year + 1));
+    }
+
+}
+
